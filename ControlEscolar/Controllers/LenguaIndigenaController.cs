13e6ab9afba44528575ea3a6a3af98550771f23a
Iridero <45569252+Iridero@users.noreply.github.com>
2021-03-24 10:51:32 -0600

    Lista lengua indigena

---
+++ ControlEscolar/Controllers/LenguaIndigenaController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ControlEscolar.Models;
+using ControlEscolar.Repositories;
+
+namespace ControlEscolar.Controllers
+{
+    public class LenguaIndigenaController : Controller
+    {
+        sieContext Context;
+        public LenguaIndigenaController(sieContext context)
+        {
+            Context = context;
+        }
+        public IActionResult Index()
+        {
+            LenguaIndigenaRepository LIRepo = new LenguaIndigenaRepository(Context);
+            return View(LIRepo.GetAll());
+        }
+        [HttpGet]
+        public IActionResult Agregar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Agregar( Lenguaindigena lengua)
+        {
+            return View();
+        }
+    }    
+}
