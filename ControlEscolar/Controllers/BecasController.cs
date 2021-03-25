040a185c9ac96cd25a55264ea87f97d116769fe2
Nayla Garcia <nayla.garcia@r2devpros.com>
2021-03-25 02:07:45 -0600

    modificaciones al diseño

---
+++ ControlEscolar/Controllers/BecasController.cs
@@ -0,0 +1,37 @@
+﻿using ControlEscolar.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControlEscolar.Controllers
+{
+    public class BecasController : Controller
+    {
+
+        sieContext Context;
+        public BecasController(sieContext context)
+        {
+            Context = context;
+        }
+
+        public IActionResult Index()
+        {
+   
+            return View();
+        }
+
+        [HttpGet]
+        public IActionResult Agregar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Agregar(Becas beca)
+        {
+            return View();
+        }
+       
+    }
+}
---
+++ ControlEscolar/Controllers/EstadosController.cs
@@ -0,0 +1,33 @@
+﻿using ControlEscolar.Models;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ControlEscolar.Controllers
+{
+    public class EstadosController : Controller
+    {
+        sieContext Context;
+        public EstadosController(sieContext context)
+        {
+            Context = context;
+        }
+        public IActionResult Index()
+        {
+            
+            return View();
+        }
+        [HttpGet]
+        public IActionResult Agregar()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Agregar(Estados estado)
+        {
+            return View();
+        }
+    }
+}
